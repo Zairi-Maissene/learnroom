8654514f1cf4d14ef2c4417ad53d46c22c7de7a2
Maissene Zairi <maissen.zairi@gmail.com>
2023-12-18 21:33:53 +0100

    Added modules and test modal button.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,35 @@
 import {NgModule} from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserModule} from "@angular/platform-browser";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {ReactiveFormsModule} from "@angular/forms";
 import {ToastrModule} from "ngx-toastr";
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FooterComponent} from './components/templates/footer/footer.component';
 import {HeaderComponent} from './components/templates/header/header.component';
 import {CommonModule} from "@angular/common";
 import {HttpClientModule} from "@angular/common/http";
+import { AssignmentFormComponent } from './modals/assignment-form/assignment-form.component';
+import { ClassroomFormComponent } from './modals/classroom-form/classroom-form..component';
+import { ClassroomIdComponent } from './modals/classroom-id/classroom-id.component';
+import { CourseFormComponent } from './modals/course-form/course-form.component';
+import { ErrorModalComponent } from './modals/error-modal/error-modal.component';
+import { StudentFormComponent } from './modals/student-form/student-form.component';
+import { TaskFormComponent } from './modals/task-form/task-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    AssignmentFormComponent,
+    ClassroomFormComponent,
+    ClassroomIdComponent,
+    CourseFormComponent,
+    ErrorModalComponent,
+    StudentFormComponent,
+    TaskFormComponent,
   ],
   imports: [
     CommonModule,
