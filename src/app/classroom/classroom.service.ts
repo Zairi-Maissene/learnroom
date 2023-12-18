762c4c1c73c59334500f9012f8f72196ec4cf37e
dhoui <mdhouib195@gmail.com>
2023-12-18 16:30:19 +0100

    adding classroom service content

--- src/app/classroom/classroom.service.ts
+++ src/app/classroom/classroom.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Student, Teacher} from "../app.service";
+import {ApiService} from "../../helpers/helpers";
 
 export type Classroom = {
   id: string,
@@ -19,6 +20,39 @@ export type UpdateClassroom = Partial<CreateClassroom>
   providedIn: 'root'
 })
 export class ClassroomService {
+  constructor(private api:ApiService<Classroom>) { }
+  // Classroom
+  addClassroom(classroom: CreateClassroom) {
+    return this.api.post(`/classroom`, classroom)
+  }
+  getClassroom(id: string) {
+    return this.api.get(`/classroom/${id}`)
+  }
+  updateClassroom(id: string, classroom: UpdateClassroom) {
+    return this.api.patch(`/classroom/${id}`, classroom)
+  }
+  deleteClassroom(id: string) {
+    return this.api.remove(`/classroom/${id}`)
+  }
 
-  constructor() { }
+  // Course
+  getCourses(classroom_id: string) {
+    return this.api.get(`/classroom/course/${classroom_id}`)
+  }
+  // Task
+  getTasks(classroom_id: string) {
+    return this.api.get(`/classroom/task/${classroom_id}`)
+  }
+  // Assignment
+  getAssignments(classroom_id: string) {
+    return this.api.get(`/classroom/assignment/${classroom_id}`)
+  }
+  // User
+  getUsers(classroom_id: string) {
+    return this.api.get(`/classroom/users/${classroom_id}`)
+  }
+  addUser(classroom_id: string, email: string) {
+    return this.api.patch(`/classroom/${classroom_id}/${email}`, {})
+  }
+
 }
