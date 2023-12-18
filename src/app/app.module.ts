fdb15e185e0aedee9d3e84647f9e3057624e511b
dhoui <mdhouib195@gmail.com>
2023-12-18 14:16:27 +0100

    adding helpers and toaster

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ToastrModule} from "ngx-toastr";
 import { TaskDetailsComponent } from './task/task-details/task-details.component';
 import { AssignementDetailsComponent } from './assignment/assignement-details/assignement-details.component';
 import { CourseDetailsComponent } from './course/course-details/course-details.component';
-import {ClassroomCardComponent} from "./components/Cards/classroom-card/classroom-card.component";
 
 @NgModule({
   declarations: [
@@ -15,13 +16,18 @@ import {ClassroomCardComponent} from "./components/Cards/classroom-card/classroo
     AppComponent,
     TaskDetailsComponent,
     AssignementDetailsComponent,
-    CourseDetailsComponent,
-    ClassroomCardComponent
+    CourseDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 5000,
+      positionClass: 'toast-top-right'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
