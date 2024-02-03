83898bab9112e217215e330136ad09404eede44b
farah seddik <farahseddik500@gmail.com>
2024-02-03 18:40:39 +0100

    added loading to lists

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, inject, Input, Output, TemplateRef, ViewChild} from '@angular/core';
 import {BehaviorSubject} from "rxjs";
 import {Course} from "@core/models/course.model";
 import {Assignement} from "@core/models/assignment.model";
@@ -11,18 +11,21 @@ import {Task} from "@core/models/task.model";
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SidebarComponent {
 
   @Output() toggle = new EventEmitter<void>();
   @Input() isCollapsed: boolean = true;
+  loading: boolean = true;
 
   data$ : BehaviorSubject<{courses:Course[][],tasks:Task[][],assignments:Assignement[][]}> = new BehaviorSubject<{courses:Course[][],tasks:Task[][],assignments:Assignement[][]}>({ courses:[], tasks: [], assignments: [] });
   courses$: BehaviorSubject<Course[]> = new BehaviorSubject<Course[]>([]);
   tasks$: BehaviorSubject<Task[]> = new BehaviorSubject<Task[]>([]);
   assignments$: BehaviorSubject<Assignement[]> = new BehaviorSubject<Assignement[]>([]);
-
+  
+  
   authService = inject(AuthPersistence);
   connService = inject(AuthService);
   currentUser$ = this.authService.user$
@@ -30,6 +33,7 @@ export class SidebarComponent {
   constructor(private router: Router) {}
 
   ngOnInit():void{
+
     this.connService.getAll().subscribe((data) => {
 
       this.data$.next(data);
@@ -40,9 +44,13 @@ export class SidebarComponent {
       this.tasks$.next(allTasks);
       const allAssignments: Assignement[] = ([] as Assignement[]).concat(...data.assignments);
       this.assignments$.next(allAssignments);
+      this.loading=false;
+      this.loading = this.loading; 
 
     })
   }
+
+
 
   toggleSidebar(): void {
     this.toggle.emit();
